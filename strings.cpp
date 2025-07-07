2e96d8f37ac41a250378cbccbe23836fe0049231
sg182 <swarnamoyghosh@Swarnamoys-MacBook-Pro.local>
2025-07-06 22:09:30 -0500

    initial commit

---
+++ strings.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    string strn = "Hello";
+    char c;
+    int size = sizeof(strn);
+
+    c = strn[1];
+    cout << "The element is "<<c<<"\n";
+    cout << "The size of the string "<<strn.size()<<"\n";
+
+
+
+
+
+}
