5cb537825a861dd0f0fd799d9fcccb183b9e6d6a
sg182 <swarnamoyghosh@Swarnamoys-MacBook-Pro.local>
2025-07-23 19:18:18 -0500

    fixed a bug of Binary search

--- rotated_sortedarray.py
+++ rotated_sortedarray.py
@@ -15,7 +15,7 @@ class Solution(object):
             if nums[mid] == target:
                 return mid
             
-            if nums[L] < nums[mid]:
+            if nums[L] <= nums[mid]:
                 #Then the L sector is sorted and the target is within this sector
                 if nums[L] <= target < nums[mid]:
                     R = mid -1
@@ -36,8 +36,9 @@ if __name__ == "__main__":
 
     sol = Solution()
 
-    nums = [4,5,6,7,0,1,2]
-    target = 3
+    #nums = [4,5,6,7,0,1,2]
+    nums = [3,1]
+    target = 1
 
     indx = sol.search(nums,target)
     print(indx)
